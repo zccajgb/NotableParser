aaf1d84cded1135288135c29ba8b9a97c16f6731
Joe Buckley <josephgbuckley@gmail.com>
2019-10-01 12:25:11 +0100

    changes to program.cs

--- BoostnoteParser/Program.cs
+++ BoostnoteParser/Program.cs
@@ -9,9 +9,9 @@ namespace BoostnoteParser
         {
             //var path = @"C:\Users\josep\Boostnote\notes\1887b24c-fca2-4d57-8fa1-8d7796ed5870.cson";
             var notesFolderPath = @"C:\Users\josep\Dropbox\PhD\Notes\notes";
-            var todoNotePath = @"C:\Users\josep\Dropbox\PhD\Notes\notes\bd3e51c5-e31f-4b0a-a5c2-b4b445ce94b1.cson";
+            var todoNotePath = @"C:\Users\josep\Dropbox\PhD\Notes\notes\Todo.md";
 
-            var defnNotePath = @"C:\Users\josep\Dropbox\PhD\Notes\notes\053e64c0-29b9-4338-b2d5-f1f60e19cbf0.cson";
+            var defnNotePath = @"C:\Users\josep\Dropbox\PhD\Notes\notes\Definitions.md";
 
             var todoProcessor = new TodoProcessor(notesFolderPath, todoNotePath);
             todoProcessor.ProcessTodos();
